655ace47ffb5f33aa1beb2e8ce65120394e84ff9
letter <letter1sent@gmail.com>
2022-07-13 10:48:10 +0900

    new 1files

--- 20220703/Main7568.java
+++ 20220703/Main7568.java
@@ -1,3 +1,4 @@
+
 public class Main7568 {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -6,10 +7,10 @@ public class Main7568 {
 
         int[][] arr = new int[N][2];
         String[] sp;
-        
-        for(int i=0;i<N;i++) {
+
+        for (int i = 0; i < N; i++) {
             sp = br.readLine().split(" "); // 입력 받은 문자열 분리
-            arr[i][0] = Integer.parseInt()
+            arr[i][0] = Integer.parseInt();
         }
     }
 }
--- 20220710/Main2231.java
+++ 20220710/Main2231.java
@@ -1,6 +1,6 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+// import java.io.IOException;
 
 public class Main2231 {
     public static void main(String[] args) {
